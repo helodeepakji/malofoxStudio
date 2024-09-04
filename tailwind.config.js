763a810880f8de34460bc6556b8cb30af669f00a
dhanushsurendra <bob@example.com>
2024-09-04 19:00:10 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: '480px',
+      lg: '976px',
+      md: '768px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        'custom-pink': '#FF01FB',
+      },
+      fontFamily: {
+        'squada': ['"Squada One"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
